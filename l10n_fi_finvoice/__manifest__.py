dcc5a8467865bf06535a7804b7edb0a77261e38e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-10-27 16:30:16 +0300

    Add a placeholder/example invoice

--- l10n_fi_finvoice/__manifest__.py
+++ l10n_fi_finvoice/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Account invoice Finvoice',
     'summary': 'Converts between Odoo invoice and Finvoice 2.00',
-    'version': '10.0.0.1.0',
+    'version': '10.0.0.1.1',
     'category': 'Accounting & Finance',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
