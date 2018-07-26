f5989765a0244f7f9da1eace5ea6f02de2b38342
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-07-26 10:20:42 +0300

    Add Opus Capita Group Oy as deactivated operator for better backwards compatibility

--- l10n_fi_edicode/models/res_partner_operator_einvoice.py
+++ l10n_fi_edicode/models/res_partner_operator_einvoice.py
@@ -5,10 +5,15 @@ from odoo import fields, models
 class ResPartnerOperatorEinvoice(models.Model):
     _name = 'res.partner.operator.einvoice'
 
+    active = fields.Boolean(
+        default=True,
+    )
+
     name = fields.Char(
         string='Operator',
         required=True,
     )
+
     identifier = fields.Char(
         string='Identifier',
         required=True,
