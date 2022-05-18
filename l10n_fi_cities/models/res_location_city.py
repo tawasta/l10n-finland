f3837dcec6b1cc345084752481431cba50882e7a
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-05-18 15:01:36 +0300

    Modified modules

---
+++ l10n_fi_cities/models/res_location_city.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class ResLocationCity(models.Model):
+    _name = "res.location_city"
+    _description = "Cities"
+
+    name = fields.Char(string="Name")
+    display_name = fields.Char(string="Display Name")
+    country_id = fields.Many2one(comodel_name="res.country", string="Country")
+    state_id = fields.Many2one(comodel_name="res.country.state", string="State")
---
+++ l10n_fi_cities_latlng/models/res_city.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class ResCityLatLong(models.Model):
+    _inherit = 'res.location_city'
+
+    latitude = fields.Float(default="0.0")
+    longitude = fields.Float(default="0.0")
