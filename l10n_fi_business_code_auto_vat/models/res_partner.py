3d892f236a78db384a18e9f65db214369b798d0c
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-08-28 15:34:03 +0300

    [ADD] Automatically fill business code when vat code is known

--- l10n_fi_business_code_auto_vat/models/res_partner.py
+++ l10n_fi_business_code_auto_vat/models/res_partner.py
@@ -1,67 +1,40 @@
-##############################################################################
-#
-#    Author: Oy Tawasta OS Technologies Ltd.
-#    Copyright 2021- Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
-#
-##############################################################################
-
-# 1. Standard library imports:
-
-# 2. Known third party imports:
-
-# 3. Odoo imports (openerp):
 from odoo import api, models
-
-# 4. Imports from Odoo modules:
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
 
 
 class ResPartner(models.Model):
-    # 1. Private attributes
     _inherit = "res.partner"
 
-    # 2. Fields declaration
-
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-
-    # 5. Constraints and onchanges
-    @api.onchange("business_id", "country_id")
-    def onchange_business_id_update_vat(self):
+    @api.onchange("business_code", "country_id")
+    def onchange_business_code_update_vat(self):
         for record in self:
-            if (
-                record.business_code
-                and record.country_id
-                and record.country_id.code in ["FI", "SE"]
-            ):
-                # Construct the VAT code:
-                # Country code + business id without dash
-                vat = "%s%s" % (
-                    record.country_id.code,
-                    record.business_code.replace("-", ""),
-                )
+            if record.country_id and record.country_id.code in ["FI", "SE"]:
 
-                record.vat = vat
+                if record.business_code and not record.vat:
+                    # Construct the VAT code:
+                    # Country code + business id without dash
+                    vat = "{}{}".format(
+                        record.country_id.code,
+                        record.business_code.replace("-", ""),
+                    )
 
-    # 6. CRUD methods
+                    record.vat = vat
+                elif record.vat and not record.business_code:
+                    # This is for create and write
+                    vat = record.vat
+                    record.business_code = "{}-{}".format(vat[2:-1], vat[-1:])
 
-    # 7. Action methods
+    def write(self, vals):
+        res = super().write(vals)
+        if vals.get("vat") or vals.get("business_code"):
+            self.record.onchange_business_code_update_vat()
 
-    # 8. Business methods
+        return res
+
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+
+        if vals.get("vat") or vals.get("business_code"):
+            res.onchange_business_code_update_vat()
+
+        return res
