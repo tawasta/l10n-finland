499bb446acd8fa3f10013d6a2d45b7c1ad893626
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-01-10 15:25:36 +0200

    [MIG] 17.0 Init

--- l10n_fi_states_latlng/models/res_country_state.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ResCountryStateLatLong(models.Model):
-
-    _inherit = "res.country.state"
-
-    latitude = fields.Float(default="0.0")
-    longitude = fields.Float(default="0.0")
