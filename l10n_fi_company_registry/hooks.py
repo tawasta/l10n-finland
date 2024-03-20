dd3ace066e06310296818f23839f83eabf960c9a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-03-20 11:37:51 +0200

    [FIX] Don't throw error if business_code column doesn't exist

--- l10n_fi_company_registry/hooks.py
+++ l10n_fi_company_registry/hooks.py
@@ -1,9 +1,17 @@
+from psycopg2.errors import UndefinedColumn
+
+
 def post_init_hook(env):
     # Migrate data from old "business_code"-field
     # This is done in hook instead of migration script, as this is a new standalone module
-    env.cr.execute(
-        "UPDATE res_partner SET company_registry = business_code WHERE company_registry IS NULL"
-    )
+    try:
+        env.cr.execute(
+            "UPDATE res_partner SET company_registry = business_code WHERE company_registry IS NULL"
+        )
+
+    except UndefinedColumn:
+        # If column doesn't exist, do nothing
+        pass
 
     # Auto-compute company registry from VAT
     env["res.partner"]._compute_company_registry_from_vat()
